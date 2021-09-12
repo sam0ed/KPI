0635c9a4e20864a019b9beb194583bedb0dc331b
ezheviktor <89961898+ezheviktor@users.noreply.github.com>
2021-09-12 23:24:46 +0300

    Add files via upload

---
+++ OP/Лабораторна 1/Lab1(pyhon).py
@@ -0,0 +1,9 @@
+
+b=int(input("Enter the first member of infinite deteriorating geomtryc progression:"))
+q=float(input("Enter the denominator of infinite deteriorating geometric progression:"))
+
+c = b / (1 - q)
+
+print("The sum of infinite deterirating geometric progression is "+ str(c))
+input()
+
