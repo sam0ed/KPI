bb95f24c41f7d884fa788a85965f48b991f0a9ce
ezheviktor <viktor15041486@gmail.com>
2022-04-19 23:52:54 +0300

    Basic classes implemented in C#

---
+++ Programming Foundations/2semester/Lab5/Lab5(CSharp)/Lab5(CSharp)/TIntNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_CSharp_
+{
+    internal class TIntNumber
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+        public string Value { get; set; }
+        public int Base { get; set; }
+        #endregion
+
+        #region Constructors
+        public TIntNumber(string v, int b)
+        {
+            Value = v;
+            Base = 0;
+        }
+        #endregion
+
+        #region Methods
+        public static TIntNumber operator++(TIntNumber intNumber)
+        {
+            int number = Convert.ToInt32(intNumber.Value, intNumber.Base);
+            number++;
+            intNumber.Value = Convert.ToString(number, intNumber.Base);
+            return intNumber;
+        }
+        public static TIntNumber operator--(TIntNumber intNumber)
+        {
+            int number = Convert.ToInt32(intNumber.Value, intNumber.Base);
+            number--;
+            intNumber.Value = Convert.ToString(number, intNumber.Base);
+            return intNumber;
+        }
+        public int ConvertToDecimal()
+        {
+            int number = Convert.ToInt32(this.Value, this.Base);
+            return number;
+        }
+        #endregion
+    }
+}
---
+++ Programming Foundations/2semester/Lab5/Lab5(CSharp)/Lab5(CSharp)/TIntNumber16.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_CSharp_
+{
+    internal class TIntNumber16
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
