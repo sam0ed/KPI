78d9bb2a79e3b38b364fc8d573a0a16dc2b815d2
Viktor L <viktor15041486@gmail.com>
2022-10-03 14:19:28 +0300

    File generator implemented

---
+++ Algorithms and Data structures/3semester/Lab/Lab1/Generator.cs
@@ -0,0 +1,80 @@
+﻿using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace Lab1;
+
+public class Generator
+{
+    public long numberAmount;
+    public int numberSizeInBytes;
+    public int numberSizeInBits;
+
+    public string fileName = "Input";
+    private string fileType;
+
+    public Random random = new Random();
+
+    public Generator(int fileSizeInBytes, string fileType)
+    {
+        numberSizeInBytes = sizeof(long);
+        numberSizeInBits = numberSizeInBytes * 8;
+        numberAmount = (long)Math.Ceiling((double)(fileSizeInBytes / numberSizeInBytes));
+        this.fileType = fileType;
+    }
+
+    public void GenerateFile()
+    {
+        if (fileType == ".bin")
+            GenerateBinaryFile();
+        else if (fileType == ".txt")
+            GenerateTextFile();
+    }
+
+    public void GenerateBinaryFile()
+    {
+        BinaryWriter bw = new BinaryWriter(File.Open(fileName, FileMode.Create));
+
+        for (int i = 0; i < numberAmount; i++)
+        {
+            bw.Write(LongRandom(0, ulong.MaxValue));
+        }
+    }
+
+    public void GenerateTextFile()
+    {
+        StreamWriter sw = new StreamWriter(File.Open(fileName, FileMode.Create));
+        //optimize
+
+        for (int i = 0; i < numberAmount; i++)
+        {
+            sw.Write(LongRandom(0, ulong.MaxValue));//
+        }
+    }
+
+    public ulong LongRandom(ulong min, ulong max)
+    {
+        byte[] buf = new byte[numberSizeInBytes];
+        random.NextBytes(buf);
+        ulong longRand = BitConverter.ToUInt64(buf, 0);
+        longRand >>= random.Next(0, numberSizeInBits );
+
+        return ((longRand % (max - min)) + min);
+    }
+}
+
+//public class TestClass<T>
+//{
+//    public long numberAmount;
+//    public TestClass()
+//    {
+//        unsafe
+//        {
+
+//            numberAmount =  Marshal.SizeOf<T>();
+//            Console.WriteLine(numberAmount);
+//        }
+//    }
+//}
---
+++ Algorithms and Data structures/3semester/Lab/Lab1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Lab1;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the size of the file you want to generate(gb/mb/kb--min 4kb): ");
+        string size = Console.ReadLine()!;
+
+        Console.Write("Enter type of file you want to use for sorting(.txt-text/.bin-binary): ");
+        string fileType = Console.ReadLine()!;
+
+        Console.Write("How many files you want to use for sorting algorithm(up to 8): ");
+        int filesAmount = int.Parse(Console.ReadLine()!);
+
+        Generator generator = new Generator(SizeInBytes(size), fileType);
+        generator.GenerateFile();
+
+    }
+
+    static int SizeInBytes(string unconvertedSize)
+    {
+        string actualSizeArray = string.Concat(unconvertedSize.ToCharArray().Where(x => x > 47 && x < 58));
+        if (string.IsNullOrEmpty(actualSizeArray)) throw new Exception("wrong formatting");
+
+        int unitSize = int.Parse(actualSizeArray);
+        string unitMeasure = string.Concat(unconvertedSize.Except(actualSizeArray));
+
+        int sizeInBytes = unitMeasure.ToLower() switch
+        {
+            "kb" => 1024 * unitSize,
+            "mb" => 1024 * 1024 * unitSize,
+            "gb" => 1024 * 1024 * 1024 * unitSize,
+            _ => 0
+        };
+        return sizeInBytes;
+    }
+}
