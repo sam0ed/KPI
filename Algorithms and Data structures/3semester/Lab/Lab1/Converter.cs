6f8e6977f4e3e17563acbb0fa27255315eb34239
Viktor L <viktor15041486@gmail.com>
2022-10-05 18:31:46 +0300

    Program class cleared( converter created)

---
+++ Algorithms and Data structures/3semester/Lab/Lab1/Converter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    static class Converter
+    {
+        public static ulong StringToBytes(string unconvertedSize)
+        {
+            string actualSizeArray = string.Concat(unconvertedSize.ToCharArray().Where(x => x > 47 && x < 58));
+            if (string.IsNullOrEmpty(actualSizeArray)) throw new Exception("Input format incorrect");
+
+            int unitSize = int.Parse(actualSizeArray);
+            string unitMeasure = string.Concat(unconvertedSize.Except(actualSizeArray));
+
+            ulong sizeInBytes = unitMeasure.ToLower() switch
+            {
+                "b" => (ulong)unitSize,
+                "kb" => (ulong)(1024 * unitSize),
+                "mb" => (ulong)(1024 * 1024 * unitSize),
+                "gb" => (ulong)(1024 * 1024 * 1024 * unitSize),
+                _ => 0
+            };
+            return sizeInBytes;
+        }
+    }
+}
