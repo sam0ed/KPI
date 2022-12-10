8759fe86f653b689d39415fbdd76ca139cf5df79
Viktor L <viktor15041486@gmail.com>
2022-12-10 15:25:36 +0100

    everything is working except initial generation of a tree

---
+++ Algorithms and Data structures/3semester/Lab/Lab3/Controllers/Controller.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Lab3.Model;
+
+
+// Define the controller class
+namespace Lab3.Controllers;
+
+public class Controller<TK, TP> where TK : IComparable<TK>
+{
+    public static readonly Random rand = new Random();
+    private const int TreeDegree = 50;
+    private const int EntriesToGenerate = 10000;
+    private BTree<TK, TP> _btree;
+
+    public Controller()
+    {
+        // Check if the Btree file exists
+        if (File.Exists("btree.json"))
+        {
+            // Upload the Btree from the file
+            UploadBtree();
+        }
+        else
+        {
+            // Generate a new Btree
+            GenerateBtree();
+        }
+    }
+
+    public void StoreBtree()
+    {
+        // Serialize the Btree to JSON
+        string json = JsonSerializer.Serialize(_btree);
+
+        // Write the JSON to the Btree file
+        File.WriteAllText("btree.json", json);
+    }
+
+    public void UploadBtree()
+    {
+        // Read the JSON from the Btree file
+        string json = File.ReadAllText("btree.json");
+
+        // Deserialize the JSON to a Btree instance
+        _btree = JsonSerializer.Deserialize<BTree<TK, TP>>(json);
+    }
+
+    public void GenerateBtree()
+    {
+        // Generate a new Btree instance
+        _btree = new BTree<TK, TP>(TreeDegree);
+
+        // Generate 10000 random entries with integer keys and string values
+        
+        for (int i = 0; i < EntriesToGenerate; i++)
+        {
+            TK key = (TK)Convert.ChangeType(rand.Next(10000), typeof(TK));
+            TP value = (TP)Convert.ChangeType("Value " + rand.Next(10000), typeof(TP));
+            _btree.Insert(key, value);
+        }
+    }
+
+    public void AddEntry(Entry<TK, TP> entry)
+    {
+        // Add the entry to the Btree
+        _btree.Insert(entry.Key, entry.Pointer);
+    }
+
+    public Entry<TK, TP>? SearchEntry(TK key)
+    {
+        // Search for the entry with the given key
+        return _btree.Search(key);
+    }
+
+    public void DeleteEntry(TK key)
+    {
+        // Delete the entry with the given key
+        _btree.Delete(key);
+    }
+
+    public void ModifyEntry(TK key, TP newPointer)
+    {
+        // Modify the entry with the given key
+        var entry = _btree.Search(key);
+        if (entry is null)
+            throw new ArgumentException("Could not modify entry with the given key");
+        else
+            entry.Pointer = newPointer;
+    }
+}
---
+++ Algorithms and Data structures/3semester/Lab/Lab3/View/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lab3.Controllers;
+using Lab3.Model;
+
+
+namespace Lab3.View;
+
+// Define the MainWindow class
+public partial class MainWindow : Window
+{
+    private Controller<int, string> controller;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        // Create a new controller instance
+        controller = new Controller<int, string>();
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            // Get the key and value from the input fields
+            int key = int.Parse(KeyInput.Text);
+            string value = ValueInput.Text;
+
+            // Create a new entry with the given key and value
+            Entry<int, string> entry = new Entry<int, string>
+            {
+                Key = key,
+                Pointer = value
+            };
+
+            // Add the entry to the Btree
+            controller.AddEntry(entry);
+
+            // Clear the input fields
+            KeyInput.Clear();
+            ValueInput.Clear();
+        }
+        catch (FormatException)
+        {
+            // Show an error message if the key is not an integer
+            MessageBox.Show("Please enter a valid integer for the key.");
+        }
+    }
+
+    private void SearchButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            // Get the key from the input field
+            int key = int.Parse(KeyInput.Text);
+
+            // Search for the entry with the given key
+            var entry = controller.SearchEntry(key);
+
+            // Show the value of the entry, or an error message if the entry was not found
+            if (entry is null)
+                MessageBox.Show("No entry found with the given key.");
+            else
+                MessageBox.Show(entry.Pointer);
+        }
+        catch (FormatException)
+        {
+            // Show an error message if the key is not an integer
+            MessageBox.Show("Please enter a valid integer for the key.");
+        }
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            // Get the key from the input field
+            int key = int.Parse(KeyInput.Text);
+
+            // Delete the entry with the given key
+            controller.DeleteEntry(key);
+
+            // Clear the input field
+            KeyInput.Clear();
+        }
+        catch (FormatException)
+        {
+            // Show an error message if the key is not an integer
+            MessageBox.Show("Please enter a valid integer for the key.");
+        }
+    }
+
+    private void ModifyButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            // Get the key and value from the input fields
+            int key = int.Parse(KeyInput.Text);
+            string value = ValueInput.Text;
+
+            // Modify the entry with the given key
+            controller.ModifyEntry(key, value);
+
+            // Clear the input fields
+            KeyInput.Clear();
+            ValueInput.Clear();
+        }
+        catch (FormatException)
+        {
+            // Show an error message if the key is not an integer
+            MessageBox.Show("Please enter a valid integer for the key.");
+        }
+        catch (ArgumentException ex)
+        {
+            // Show an error message if the entry could not be modified
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        // Store the Btree to the file when the window is closing
+        controller.StoreBtree();
+    }
+}
