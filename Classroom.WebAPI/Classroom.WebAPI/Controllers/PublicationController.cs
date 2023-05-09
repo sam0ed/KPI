92fbc7e17675c42401baf4d568228d02db9aa22e
samoed <viktor15041486@gmail.com>
2023-05-09 19:19:53 +0200

    Finished API

--- Classroom.WebAPI/Classroom.WebAPI/Controllers/PublicationController.cs
+++ Classroom.WebAPI/Classroom.WebAPI/Controllers/PublicationController.cs
@@ -5,10 +5,11 @@ using Swashbuckle.AspNetCore.Annotations;
 namespace Classroom.WebAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("ClassRoom/{classroomId}/[controller]")]
 public class PublicationController:ControllerBase
 {
-    [HttpGet("{classroomId}/publications")]
+
+    [HttpGet]
     [SwaggerResponse(200, "Got all assignments of the given classroom successfully")]
     [SwaggerResponse(404, "Not found such classroom with specified id")]
     public async Task<ActionResult<List<MaterialDTO>>> GetPublications(int classroomId)
@@ -16,7 +17,7 @@ public class PublicationController:ControllerBase
         return Ok();
     }
     
-    [HttpGet("{classroomId}/publications/assignment/{assignmentId}")]
+    [HttpGet("Assignment/{assignmentId}")]
     [SwaggerResponse(200, "Entered the publication page successfully")]
     [SwaggerResponse(404, "Not found such classroom or assignment with specified id")]
     public async Task<ActionResult<AssignmentDTO>> GetAssignment(int classroomId, int assignmentId)
@@ -24,7 +25,7 @@ public class PublicationController:ControllerBase
         return Ok();
     }
     
-    [HttpGet("{classroomId}/publications/material/{materialId}")]
+    [HttpGet("Material/{materialId}")]
     [SwaggerResponse(200, "Entered the publication page successfully")]
     [SwaggerResponse(404, "Not found such classroom or material with specified id")]
     public async Task<ActionResult<MaterialDTO>> GetMaterial(int classroomId, int materialId)
@@ -32,35 +33,35 @@ public class PublicationController:ControllerBase
         return Ok();
     }
     
-    [HttpPost("{classroomId}/publications/assignment")]
+    [HttpPost("Assignment")]
     [SwaggerResponse(201, "Assignment was created successfully")]
     public async Task<IActionResult> PostAssignment(int classroomId,[FromBody] AssignmentDTO assignment)
     {
         return Ok();
     }
     
-    [HttpPost("{classroomId}/publications/material")]
+    [HttpPost("Material")]
     [SwaggerResponse(201, "Material was created successfully")]
     public async Task<IActionResult> PostMaterial(int classroomId,[FromBody] MaterialDTO material)
     {
         return Ok();
     }
     
-    [HttpPut("{classroomId}/publications/assignment/{assignmentId}")]
+    [HttpPut("Assignment/{assignmentId}")]
     [SwaggerResponse(200, "Assignment updated successfully")]
     public async Task<ActionResult<AssignmentDTO>> UpdateAssignment(int classroomId,int assignmentId,[FromBody] AssignmentDTO assignment) //
     {
         return Ok();
     }
     
-    [HttpPut("{classroomId}/publications/material/{materialId}")]
+    [HttpPut("Material/{materialId}")]
     [SwaggerResponse(200, "Material updated successfully")]
     public async Task<ActionResult<MaterialDTO>> UpdateMaterial(int classroomId,int materialId,[FromBody] MaterialDTO material) //
     {
         return Ok();
     }
     
-    [HttpDelete("{classroomId}/publications")]
+    [HttpDelete]
     [SwaggerResponse(204, "Deleted publication with specified id")]
     [SwaggerResponse(404, "Not found such publication with specified ids")]
     public async Task<IActionResult> DeletePublication(int publicationId)
---
+++ Classroom.WebAPI/Classroom.WebAPI/Controllers/StudentWorkController.cs
@@ -0,0 +1,36 @@
+﻿using Classroom.WebAPI.Models.Publication;
+using Classroom.WebAPI.Models.Student;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Classroom.WebAPI.Controllers;
+
+[ApiController]
+[Route("ClassRoom/{classroomId}/Publication/Assignment/{assignmentId}/[controller]")]
+public class StudentWorkController: ControllerBase
+{
+    //No put or post endpoints because empty attachments are present for all of the students by default
+    [HttpGet]
+    [SwaggerResponse(200, "Got all attachments of the given assignment successfully")]
+    [SwaggerResponse(404, "Not found such assignment or classroom with specified id")]
+    public async Task<ActionResult<List<StudentWorkDTO>>> GetAllStudentWork(int classroomId, int assignmentId)
+    {
+        return Ok();
+    }
+    
+    [HttpGet("{studentWorkId}")]
+    [SwaggerResponse(200, "Got attachment for the given assignment of the given student successfully")]
+    [SwaggerResponse(404, "Not found such assignment or classroom with specified id")]
+    public async Task<ActionResult<StudentWorkDTO>> GetStudentWork(int classroomId, int assignmentId, int studentId)
+    {
+        return Ok();
+    }
+
+    [HttpPut("{studentWorkId}")]
+    [SwaggerResponse(200, "Attachment updated successfully")]
+    public async Task<ActionResult<StudentWorkDTO>> UpdateStudentWork(int classroomId, int assignmentId, int studentId,
+        [FromBody] StudentWorkDTO attachment)
+    {
+        return Ok();
+    }
+}
