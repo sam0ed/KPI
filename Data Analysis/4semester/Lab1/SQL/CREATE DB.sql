702a9ad96fc7862e3ea9e816f8e8b01dd1d9cd0b
sam0ed <samoed.prime@gmail.com>
2023-11-19 10:52:59 +0100

    restructuring

--- Data Analysis/4semester/Lab1/SQL/CREATE DB.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE you_tube_analysis;
-
-CREATE USER defaultUser WITH PASSWORD '1111';
-
-DROP DATABASE you_tube_analysis
--- Data Analysis/4semester/Lab1/script/__main__.py
+++
@@ -1,8 +0,0 @@
-from extract.main import main as extract_main
-from transform.main import main as transform_main
-
-extract_main()
-transform_main()
-
-
-
